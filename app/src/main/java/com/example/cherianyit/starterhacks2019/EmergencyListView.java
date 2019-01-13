c4555b8c2091cad7969fcac34127862315280db2
cherianyit <cheriany@invaware.com>
2019-01-13 03:33:35 -0500

    switch between activities now functional

--- app/src/main/java/com/example/cherianyit/starterhacks2019/EmergencyListView.java
+++ app/src/main/java/com/example/cherianyit/starterhacks2019/EmergencyListView.java
@@ -14,19 +14,28 @@ import android.os.Bundle;
 import android.content.Context;
 import android.view.View;
 import android.view.View.OnClickListener;
-
-
 import android.widget.ImageButton;
 
 public class EmergencyListView extends AppCompatActivity {
+
+    private ImageButton button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency_list_view);
+
+        button = (ImageButton) findViewById(R.id.imageButton_topright);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                launchActivity(view);
+            }
+        });
     }
 
-    private  void lauchActivity() {
+    private void launchActivity(View view) {
         Intent intent = new Intent(this, MapsView.class);
         startActivity(intent);
     }
